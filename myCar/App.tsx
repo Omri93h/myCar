3ce2f18ee419119c78c63eac1b628465b117386a
Omri93h <omri93h@gmail.com>
2023-06-01 22:10:02 +0300

    Adding icons

--- myCar/App.tsx
+++ myCar/App.tsx
@@ -1,32 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-// import type { PropsWithChildren } from 'react';
+
 import {
   Dimensions,
   Image,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
   TextInput,
-  useColorScheme,
   View,
 } from 'react-native';
-import LinearGradient from 'react-native-linear-gradient'
-
-
-
-// type SectionProps = PropsWithChildren<{
-//   title: string;
-// }>;
-
+import LinearGradient from 'react-native-linear-gradient';
 
 const windowHeight = Dimensions.get('window').height;
 
@@ -35,19 +18,33 @@ function App(): JSX.Element {
     <SafeAreaView style={styles.backGround}>
       <View style={styles.rootContainer}>
         <View style={styles.headerContainer}>
-          {/* <Image style={styles.icon} source={require('./logo.png')} /> */}
-          <Text style={styles.headerText}>My Timer</Text>
+          <Image style={styles.icon} source={require('./logo.png')} />
         </View>
-        <View style={styles.loginContainer}>
-          <Text style={styles.loginText}>To access the app,{"\n"}Please enter your information below</Text>
-          <TextInput style={styles.input} placeholder="Email"></TextInput>
-          <TextInput style={styles.input} placeholder="Phone Number"></TextInput>
 
+        <View style={styles.loginContainer}>
+          <Text style={styles.loginText}>
+            To access the app,{'\n'}Please enter your information below
+          </Text>
+          <View style={styles.inputContainer}>
+            <Image style={styles.inputIcon} source={require('./src/img/messageSquare.png')} />
+            <TextInput style={styles.input} placeholder="Email"></TextInput>
+          </View>
+          <View style={styles.inputContainer}>
+            <Image style={styles.inputIcon} source={require('./src/img/callSquare.png')} />
+            <TextInput style={styles.input} placeholder="Phone Number"></TextInput>
+          </View>
         </View>
+
         <View style={styles.helpContainer}>
-          <Text style={styles.helpText}>Need help?{"\n"}Contact us!</Text>
-          <LinearGradient colors={['blue', 'magenta']} style={styles.linearGradient} start={{ x: 0.2, y: 0.1 }}>
-          <Image style={styles.arrowImage} source={require('./src/img/arrow.png')}/>
+          <Text style={styles.helpText}>Need help?{'\n'}Contact us!</Text>
+          <LinearGradient
+            colors={['#fdfd96', '#c32148']}
+            style={styles.linearGradient}
+            start={{ x: 0.1, y: 0.1 }}>
+            <Image
+              style={styles.arrowImage}
+              source={require('./src/img/arrow.png')}
+            />
           </LinearGradient>
         </View>
       </View>
@@ -56,63 +53,62 @@ function App(): JSX.Element {
 }
 
 const styles = StyleSheet.create({
-
   backGround: {
     flex: 1,
-    // backgroundColor: 'pink',
     borderWidth: 4,
-    // borderColor: 'green',
   },
   rootContainer: {
     borderWidth: 2,
-    // borderColor: 'green',
     flex: 1,
-    // backgroundColor: "yellow",
-
   },
   headerContainer: {
     width: '100%',
     height: windowHeight * 0.1,
-    backgroundColor: '#eee',
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
     shadowColor: 'black',
-    shadowOffset: { width: 1, height: 5 },
-    shadowRadius: 3,
-    shadowOpacity: 0.2,
-    zIndex: 1
+    shadowOffset: { width: 1, height: 3 },
+    shadowRadius: 1,
+    shadowOpacity: 0.3,
+    zIndex: 1,
   },
   loginContainer: {
     width: '100%',
     height: windowHeight * 0.4,
-    // backgroundColor: 'red',
     justifyContent: 'center',
     alignItems: 'center',
     fontSize: 18,
+    // backgroundColor:'green'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+    borderWidth: 1,
+    borderColor: '#aaa',
+    height: 40,
+    borderRadius: 5,
+    margin: 10,
   },
   helpContainer: {
     width: '100%',
     height: windowHeight * 0.2,
-    // backgroundColor: 'magenta',
     alignItems: 'center',
     flexDirection: 'row',
-    justifyContent:'space-around'
-
+    justifyContent: 'space-around',
   },
   input: {
     height: 40,
-    width: '80%',
+    width: '75%',
     margin: 12,
-    borderWidth: 1,
+    // borderWidth: 1,
     padding: 10,
-  },
-  headerText: {
-    fontSize: 40,
-    fontFamily: "American Typewriter"
   },
   loginText: {
     fontSize: 15,
+    padding: 20
   },
   helpText: {
     fontSize: 20,
@@ -121,26 +117,32 @@ const styles = StyleSheet.create({
     fontSize: 20,
     color: 'blue',
     textDecorationLine: 'underline',
-
   },
   icon: {
-    width: 40,
-    height: 40,
+    width: 175,
+    height: 50,
   },
   arrowImage: {
     width: 50,
     height: 50,
-    borderRadius:50,
-    transform: [{ rotate: '180deg'}]
+    borderRadius: 50,
+    transform: [{ rotate: '180deg' }],
+  },
+  inputIcon: {
+    padding: 10,
+    margin: 5,
+    height: 25,
+    width: 25,
+    resizeMode: 'stretch',
+    alignItems: 'center',
   },
   linearGradient: {
     alignItems: 'center',
     justifyContent: 'center',
     height: 55,
     width: 55,
-    borderRadius:50
+    borderRadius: 50,
   },
-
 });
 
 export default App;
